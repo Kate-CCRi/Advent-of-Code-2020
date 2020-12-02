8f6ff7fdd74625c98ca0600a12c311f2ba3fbe4b
Kate-CCRi <ksecor@ccri.com>
2020-12-02 11:03:14 -0500

    Works with the test data!

--- Day 01/day1.py
+++ Day 01/day1.py
@@ -1,5 +1,5 @@
-# Make it so it'll run on my stupid NUC, which has Python 2.7 instead of 3.6
-from __future__ import print_function
+# Import itertools for better iteration
+import itertools
 
 # read in the data file and split it into lines
 data = open("test.txt")
@@ -7,18 +7,15 @@ lines = data.readlines()
 
 # PART ONE
 
-# Set num1 to the first item in the "lines" file
-num1 = int(lines.pop())
-num2 = 0
+for num1, num2 in itertools.combinations(lines, 2):
+    sum = int(num1) + int(num2)
 
-for line in lines:
-    num2 = int(line)
-
-    if num1 + num2 == 2020:
+    if sum == 2020:
+        product = int(num1) * int(num2)
+        print(f"The first number is {num1}")
+        print(f"The second number is {num2}")
+        print(f"Their sum is {sum}")
+        print(f"Their product is {product}")
         break
 
-product = num1 * num2
 
-print(f"The first number is {num1}")
-print(f"The second number is {num2}")
-print(f"Their product is {product}")
