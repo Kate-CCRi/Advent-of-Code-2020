017fcfaced7b055edf5043d01db210a04262d586
Kate-CCRi <ksecor@ccri.com>
2020-12-03 11:33:54 -0500

    Working Day 3.1 code.

---
+++ Day 03/day3.py
@@ -0,0 +1,29 @@
+# read in the data file and split it into lines
+data = open("data.txt")
+lines = data.readlines()
+
+# Set up some counters
+xpos = 0
+ypos = 0
+trees = 0
+# This one had to be -1 because the readlines was including the newline character
+linesize = len(lines[0]) - 1
+
+# Iterate through the data file
+
+for line in lines:
+
+# If we wrap around the end of the line, start again at the beginning
+    if xpos >= linesize:
+        xpos = xpos - linesize
+
+# If you hit a tree, increment the tree counter
+    if line[xpos] == "#":
+        trees += 1
+
+# Do the moves
+    xpos += 3
+    ypos += 1
+
+# Print the answer
+print(f"The number of trees you hit is {trees}.")
