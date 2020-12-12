cd3d789fbb5082545604685b246cc0c67f0c26af
Kate-CCRi <ksecor@ccri.com>
2020-12-11 22:31:59 -0500

    Well, I'm getting somewhere, kinda, but the splitter code for the individual lines needs a rewrite. Put in a suggested approach.

--- Day 04/day4.py
+++ Day 04/day4.py
@@ -4,3 +4,26 @@ lines = data.readlines()
 
 print(lines)
 
+'''
+Approach:
+    * pull items off the list of lines until you get to a newline
+        lines.pop(0) will get you the first thing in a list
+    * read the things you've now got into a k,v dictionary for each person
+    * check the list of keys vs. the list of all the keys you're supposed to have
+    * if you find them all, increment a counter
+'''
+passwords = []
+
+for line in lines:
+    if line != '\n':
+        break
+        # This code doesn't work - it wants to iterate by single characters - need to put in a "split on :" kind of deal instead and then put the first thing in as the key and the second thing in as the value
+        # for item in line:
+        #    print(item)
+        #    data = {}
+        #    data[item[0]] = item[2]
+        # passwords.append(data)
+    else:
+        print("I found a naked newline!")
+
+print(passwords)
