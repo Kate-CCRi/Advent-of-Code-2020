97f001fc45ffb1405cd8a5be9147b2ddc40efc8c
Kate-CCRi <ksecor@ccri.com>
2020-12-12 20:47:28 -0500

    Updated Day4 code that splits the data properly but doesn't do anything useful with it.

--- Day 04/day4.py
+++ Day 04/day4.py
@@ -12,18 +12,36 @@ Approach:
     * check the list of keys vs. the list of all the keys you're supposed to have
     * if you find them all, increment a counter
 '''
-passwords = []
+
+newdata = []
+passports = {}
 
 for line in lines:
+
+    # Each set of valid data is separated by a naked newline, so we can assume that data between newlines is all one set of information.
+    # Check to make sure tht your line isn't a new line:
     if line != '\n':
-        break
-        # This code doesn't work - it wants to iterate by single characters - need to put in a "split on :" kind of deal instead and then put the first thing in as the key and the second thing in as the value
-        # for item in line:
-        #    print(item)
-        #    data = {}
-        #    data[item[0]] = item[2]
-        # passwords.append(data)
+
+        # This splits the line into the individual pieces of data and aggregates all the data between newlines
+        newdata += line.split()
+        print(newdata)
+
     else:
+        # Next step: Figure out how to take the aggregated newline and read it into a single list of dictionary entries per person
+
+        tempdict = {}
+
+        for item in newdata:
+
+            # Splits each item in the newdata list into a two item list at the colon
+            templist = item.split(":")
+
+            # This is *supposed* to read the items into a temporary dictionary but doesn't work for some reason
+            tempdict[templist[0]] = tempdict[templist[1]]
+
+            print(tempdict)
+
+        # Reset the newline to an empty list when you find a naked newline, which distinguishes a new person's data
+        newdata = []
         print("I found a naked newline!")
 
-print(passwords)
